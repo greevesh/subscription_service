6149f755cf0f1d69b51cb338e550dae840c86a04
Harrison Louis Greeves <hgreeves@protonmail.com>
2020-03-07 23:08:53 +0800

    Removed all the 404 console errors.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,20 +8,7 @@
     <meta name="generator" content="Jekyll v3.8.6">
     <title>Subscription Service</title>
 
-    
-    <link rel="canonical" href="https://getbootstrap.com/docs/4.4/examples/pricing/">
-
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
-    <!-- Favicons -->
-    <link rel="apple-touch-icon" href="/docs/4.4/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
-    <link rel="icon" href="/docs/4.4/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
-    <link rel="icon" href="/docs/4.4/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
-    <link rel="manifest" href="/docs/4.4/assets/img/favicons/manifest.json">
-    <link rel="mask-icon" href="/docs/4.4/assets/img/favicons/safari-pinned-tab.svg" color="#563d7c">
-    <link rel="icon" href="/docs/4.4/assets/img/favicons/favicon.ico">
-    <meta name="msapplication-config" content="/docs/4.4/assets/img/favicons/browserconfig.xml">
-    <meta name="theme-color" content="#563d7c">
 
     <style>
       .bd-placeholder-img {
@@ -39,9 +26,6 @@
         }
       }
     </style>
-    <!-- Custom styles for this template -->
-    <link href="pricing.css" rel="stylesheet">
-    <link href="app.css" rel="stylesheet">
   </head>
 
   @include('partials.nav')
